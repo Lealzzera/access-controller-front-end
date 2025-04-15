8a941a7831cfd38938b6fd1bec08e566f6cf011b
Matheus Leal <matheus.mleall@gmail.com>
2025-04-15 15:18:50 -0300

    adjusted middleware

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import verifyToken from "./app/helpers/verifyToken";
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest, res: NextResponse) {
   const { cookies } = req;
+
   const accessToken = cookies.get("access_token")?.value;
+
   const isTokenValid = await verifyToken(accessToken);
+
   if (!isTokenValid && req.nextUrl.pathname !== "/") {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
   if (isTokenValid && req.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/home", req.url));
   }
