73c303d15f2eb048e599705e7a16bac90cb29b09
Lealzzera <matheus.mleall@gmail.com>
2025-04-24 15:54:29 -0300

    Added the menu button to register a child, responsible and logout application

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,11 +4,23 @@ import style from "./style.module.css";
 import logout from "@/app/helpers/logout";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Role } from "@/app/enums/Role.enum";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Menu, MenuItem } from "@mui/material";
+import React from "react";
 
 export default function Header() {
   const router = useRouter();
   const { userInfo } = useUser();
   const [userName, setUserName] = useState<string | undefined>("");
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
 
   const logoutFunction = async () => {
     await logout();
@@ -23,7 +35,20 @@ export default function Header() {
     <header className={style.headerContainer}>
       <div className={style.wrappedContent}>
         <span>{userName}</span>
-        <p onClick={logoutFunction}>SAIR</p>
+      {userInfo?.role === Role.INSTITUTION && <div className={style.containerMenu}>
+        <button className={style.buttonMenu}  onClick={handleClick}><MenuIcon /></button>
+          <Menu
+        id="basic-menu"
+        open={open}
+        anchorEl={anchorEl}
+
+      >
+        <MenuItem>Cadastrar Criança</MenuItem>
+        <MenuItem>Cadastrar Responsável</MenuItem>
+        <MenuItem onClick={logoutFunction}>Sair</MenuItem>
+      </Menu>
+        </div>}
+      {userInfo?.role === Role.RESPONSIBLE && <p onClick={logoutFunction}>SAIR</p>}
       </div>
     </header>
   );
