cbcae75c3e8a28f82b800021fa45667dead53ef5
Lealzzera <matheus.mleall@gmail.com>
2025-05-02 12:35:29 -0300

    built the sketch and base of register responsible modal

--- app/components/ModalRegisterResponsibleComponent/ModalRegisterResponsibleComponent.tsx
+++ app/components/ModalRegisterResponsibleComponent/ModalRegisterResponsibleComponent.tsx
@@ -4,11 +4,30 @@ import { useUser } from '@/app/context/userContext';
 import style from './style.module.css';
 import { SyntheticEvent, useRef, useState } from 'react';
 import InputFieldComponent from '../InputFieldComponent/InputFieldComponent';
+import ButtonComponent from '../ButtonComponent/ButtonComponent';
+import { CircularProgress } from '@mui/material';
+import NoPhotographyRoundedIcon from '@mui/icons-material/NoPhotographyRounded';
+import Image from 'next/image';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 export default function ModalRegisterResponsibleComponent() {
   const { registerResponsibleModalOpen, setRegisterResponsibleModalOpen } = useUser();
 
+  const [loadRegisterData, setLoadRegisterData] = useState(false);
+  const [imagePreviewerData, setImagePreviewerData] = useState('');
+  const [fileName, setFileName] = useState('');
+
   const [responsibleName, setResponsibleName] = useState('');
+  const [responsibleCpf, setResponsibleCpf] = useState('');
+  const [street, setStreet] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [kinship, setKinship] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const modalBg = useRef<HTMLDivElement | null>(null);
 
@@ -22,12 +41,16 @@ export default function ModalRegisterResponsibleComponent() {
       {registerResponsibleModalOpen && (
         <div ref={modalBg} onClick={handleClickOutSide} className={style.registerModalBg}>
           <div className={style.registerModalContent}>
-            <div>
-              <h1 className={style.modalTitleMessage}>
-                Agora vamos cadastrar um responsável para o <span>FULANO.</span>
-              </h1>
+            <div className={style.closeModalIcon}>
+              <CloseRoundedIcon fontSize="large" className={style.closeIcon} />
             </div>
-            <form>
+            <div className={style.registerInformationText}>
+              <h1 className={style.modalTitle}>Cadastrar responsável</h1>
+              <p className={style.modalDescription}>
+                Agora vamos cadastrar um responsável para o <span>FULANO.</span>
+              </p>
+            </div>
+            <form className={style.registerForm}>
               <InputFieldComponent
                 idInput="name"
                 inputLabel="Nome"
@@ -36,26 +59,159 @@ export default function ModalRegisterResponsibleComponent() {
                 setInputValue={(event) => setResponsibleName(event)}
               />
               <InputFieldComponent
-                idInput="name"
-                inputLabel="Nome"
+                idInput="cpf"
+                inputLabel="CPF"
                 inputType="text"
-                inputValue={responsibleName}
-                setInputValue={(event) => setResponsibleName(event)}
+                inputValue={responsibleCpf}
+                setInputValue={(event) => setResponsibleCpf(event)}
               />
               <InputFieldComponent
-                idInput="name"
-                inputLabel="Nome"
+                idInput="zipcode"
+                inputLabel="CEP"
                 inputType="text"
-                inputValue={responsibleName}
-                setInputValue={(event) => setResponsibleName(event)}
-              />{' '}
-              <InputFieldComponent
-                idInput="name"
-                inputLabel="Nome"
-                inputType="text"
-                inputValue={responsibleName}
-                setInputValue={(event) => setResponsibleName(event)}
+                inputValue={postalCode}
+                setInputValue={(event) => setPostalCode(event)}
               />
+              <InputFieldComponent
+                idInput="street"
+                inputLabel="Rua"
+                inputType="text"
+                inputValue={street}
+                setInputValue={(event) => setStreet(event)}
+              />
+              <InputFieldComponent
+                idInput="neighborhood"
+                inputLabel="Bairro"
+                inputType="text"
+                inputValue={neighborhood}
+                setInputValue={(event) => setNeighborhood(event)}
+              />
+              <InputFieldComponent
+                idInput="city"
+                inputLabel="Cidade"
+                inputType="text"
+                inputValue={city}
+                setInputValue={(event) => setCity(event)}
+              />
+              <InputFieldComponent
+                idInput="state"
+                inputLabel="Estado"
+                inputType="text"
+                inputValue={state}
+                setInputValue={(event) => setState(event)}
+              />
+              <InputFieldComponent
+                idInput="email"
+                inputLabel="Email"
+                inputType="email"
+                inputValue={email}
+                setInputValue={(event) => setEmail(event)}
+              />
+              <InputFieldComponent
+                idInput="password"
+                inputLabel="Senha"
+                inputType="password"
+                inputValue={password}
+                setInputValue={(event) => setPassword(event)}
+              />
+              <InputFieldComponent
+                idInput="confirmPassword"
+                inputLabel="Confirmar senha"
+                inputType="password"
+                inputValue={confirmPassword}
+                setInputValue={(event) => setConfirmPassword(event)}
+              />
+
+              <div
+                style={{
+                  backgroundColor: imagePreviewerData ? 'transparent' : 'rgba(0, 0, 0, 0.4)',
+                }}
+                className={style.noPicture}
+              >
+                {!imagePreviewerData ? (
+                  <div
+                    style={{
+                      height: '200px',
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <NoPhotographyRoundedIcon
+                      style={{
+                        fontSize: '80px',
+                        color: 'rgba(0, 0, 0, 0.6)',
+                      }}
+                    />
+                  </div>
+                ) : (
+                  <Image
+                    className={style.imageSelected}
+                    width={100}
+                    height={50}
+                    alt="image"
+                    src={imagePreviewerData}
+                  />
+                )}
+              </div>
+              <p
+                style={{
+                  fontSize: '10px',
+                  visibility: fileName.length ? 'initial' : 'hidden',
+                }}
+              >
+                'nothing'
+              </p>
+              <div className={style.containerButtons}>
+                <div className={style.containerFileButton}>
+                  <label className={style.chooseFileLabel} htmlFor="inputFile">
+                    Selecionar
+                  </label>
+                  <input
+                    id="inputFile"
+                    accept="image/jpeg, image/jpg, image/png, image/webp"
+                    size={60}
+                    className={style.chooseFileButton}
+                    type="file"
+                  />
+                </div>
+                <div>
+                  <ButtonComponent
+                    style={{
+                      fontSize: '12px',
+                      textTransform: 'none',
+                      backgroundColor: 'var(--blue-400)',
+                      boxShadow: 'none',
+                      border: '1px solid var(--blue-400)',
+                    }}
+                    buttonText="Tirar foto"
+                  />
+                </div>
+              </div>
+              <div className={style.registerButtonContainer}>
+                <ButtonComponent
+                  style={{
+                    fontSize: '1rem',
+                    textTransform: 'none',
+                    backgroundColor: '#00a159',
+                    boxShadow: 'none',
+                    fontWeight: 'bold',
+                    color: '#002F1A',
+                  }}
+                  type="submit"
+                  buttonText={
+                    loadRegisterData ? (
+                      <CircularProgress
+                        size={28}
+                        style={{
+                          color: '#002F1A',
+                        }}
+                      />
+                    ) : (
+                      'Enviar'
+                    )
+                  }
+                />
+              </div>
             </form>
           </div>
         </div>
