9a337452be180824e73246e1037be2b6e5df926b
Matheus Leal <matheus.mleall@gmail.com>
2026-02-13 01:30:13 -0300

    implemented the child modal to create a child on the responsible screen

--- app/components/ModalRegisterChild/ModalRegisterChildComponent.tsx
+++ app/components/ModalRegisterChild/ModalRegisterChildComponent.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import style from './style.module.css';
 import InputFieldComponent from '../InputFieldComponent/InputFieldComponent';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import SelectComponent from '../SelectComponent/SelectComponent';
 import ButtonComponent from '../ButtonComponent/ButtonComponent';
 import NoPhotographyRoundedIcon from '@mui/icons-material/NoPhotographyRounded';
@@ -25,9 +26,16 @@ import { toast, ToastContainer } from 'react-toastify';
 import { handleStartCamera } from '@/app/helpers/handleStartCamera';
 import cleanCpfNumber from '@/app/helpers/cleanCpfNumber';
 
-export default function ModalRegisterChildComponent() {
-  const { registerModalOpen, setRegisterModalOpen, userInfo, setLastChildRegisteredInformation } =
-    useUser();
+type ModalRegisterChildComponentProps = {
+  handleBackModal: () => void;
+  isModalOpen: (value: boolean) => void;
+};
+
+export default function ModalRegisterChildComponent({
+  handleBackModal,
+  isModalOpen,
+}: ModalRegisterChildComponentProps) {
+  const { userInfo } = useUser();
 
   const [name, setName] = useState('');
   const [cpf, setCpf] = useState('');
@@ -69,11 +77,11 @@ export default function ModalRegisterChildComponent() {
 
   const handleCloseModal = () => {
     if (loadRegisterData) return;
-    setRegisterModalOpen(false);
     stream?.getTracks().forEach((track) => {
       track.stop();
     });
     resetAllStatus();
+    isModalOpen(false);
   };
 
   const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -113,11 +121,6 @@ export default function ModalRegisterChildComponent() {
       });
     }
 
-    setLastChildRegisteredInformation({
-      cpf,
-      id: responseRegister.child.id,
-      name,
-    });
     setLoadRegisterData(false);
     notifySuccess();
     handleCloseModal();
@@ -188,204 +191,207 @@ export default function ModalRegisterChildComponent() {
   return (
     <>
       <ToastContainer containerId="success-container" />
-      {registerModalOpen && (
+      <div
+        onClick={handleCloseModalClickingOutside}
+        ref={modalContainer}
+        className={`${style.modalContainer}`}
+      >
+        <ToastContainer containerId="error-container" />
+        <ModalCameraComponent
+          imagePreviewerData={imagePreviewerData}
+          isCameraModalOpen={isCameraModalOpen}
+          loadCameraData={loadCameraData}
+          setFileData={setFileData}
+          setFileName={setFileName}
+          setImagePreviewerData={setImagePreviewerData}
+          setIsCameraModalOpen={setIsCameraModalOpen}
+          setLoadCameraData={setLoadCameraData}
+          stream={stream}
+        />
         <div
-          onClick={handleCloseModalClickingOutside}
-          ref={modalContainer}
-          className={`${style.modalContainer}`}
+          style={{
+            display: isCameraModalOpen ? 'none' : 'block',
+          }}
+          className={style.modalContentWrapped}
         >
-          <ToastContainer containerId="error-container" />
-          <ModalCameraComponent
-            imagePreviewerData={imagePreviewerData}
-            isCameraModalOpen={isCameraModalOpen}
-            loadCameraData={loadCameraData}
-            setFileData={setFileData}
-            setFileName={setFileName}
-            setImagePreviewerData={setImagePreviewerData}
-            setIsCameraModalOpen={setIsCameraModalOpen}
-            setLoadCameraData={setLoadCameraData}
-            stream={stream}
-          />
-          <div
-            style={{
-              display: isCameraModalOpen ? 'none' : 'block',
-            }}
-            className={style.modalContentWrapped}
-          >
-            <div className={style.closeButtonContainer}>
-              <CloseRoundedIcon
-                onClick={handleCloseModal}
-                fontSize="large"
-                className={style.closeIcon}
-              />
-            </div>
-            <div className={style.registerInformationText}>
-              <h1 className={style.registerTitle}>Cadastrar criança</h1>
-              <p className={style.registerDescription}>
-                Vamos lá! Para cadastrar uma nova criança, basta inserir os dados abaixo.
-              </p>
-            </div>
-            <form className={style.formContainer} onSubmit={handleRegister}>
-              <InputFieldComponent
-                required={true}
-                disabled={loadRegisterData}
-                idInput="name"
-                inputLabel="Nome completo"
-                style={{
-                  textTransform: 'capitalize',
-                }}
-                setInputValue={setName}
-                inputValue={name}
-                inputType="text"
-                placeholder="Ex: João da Silva Santos"
-              />
-              <InputFieldComponent
-                disabled={loadRegisterData}
-                required={true}
-                idInput="cpf"
-                inputLabel="CPF (somente números)"
-                setInputValue={(event) => setCpf(maskCpfFunction(event))}
-                inputValue={cpf}
-                inputType="text"
-                placeholder="999.999.999-99"
-              />
-              <InputFieldComponent
-                disabled={loadRegisterData}
-                required={true}
-                idInput="birthDate"
-                inputLabel="Data de nascimento"
-                style={{
-                  textTransform: 'capitalize',
-                }}
-                setInputValue={(event) => setBirthDate(maskBirthDateFunction(event))}
-                inputValue={birthDate}
-                inputType="text"
-                placeholder="DD/MM/YYYY"
-              />
-              <SelectComponent
-                required
-                disabled={loadRegisterData}
-                selectId="period"
-                setSelectValue={setPeriod}
-                selectLabel="Período"
-                selectOptions={periodOptions}
-                selectName="period"
-                labelText="Selecione um período"
-              />
-              <SelectComponent
-                required
-                disabled={loadRegisterData}
-                selectId="grade"
-                setSelectValue={setGrade}
-                selectLabel="Turma"
-                selectOptions={gradeOptions}
-                selectName="grade"
-                labelText="Selecione uma turma"
-              />
-              <div
-                style={{
-                  backgroundColor: imagePreviewerData ? 'transparent' : 'rgba(0, 0, 0, 0.4)',
-                }}
-                className={style.noPicture}
-              >
-                {!imagePreviewerData ? (
-                  <div
-                    style={{
-                      height: '200px',
-                      display: 'flex',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <NoPhotographyRoundedIcon
-                      style={{
-                        fontSize: '80px',
-                        color: 'rgba(0, 0, 0, 0.6)',
-                      }}
-                    />
-                  </div>
-                ) : (
-                  <Image
-                    className={style.imageSelected}
-                    width={100}
-                    height={50}
-                    alt="image"
-                    src={imagePreviewerData}
-                  />
-                )}
-              </div>
-              <p
-                style={{
-                  fontSize: '10px',
-                  visibility: fileName.length ? 'initial' : 'hidden',
-                }}
-              >
-                {fileName.length ? fileName : 'nothing'}
-              </p>
-              <div className={style.containerButtons}>
-                <div className={style.containerFileButton}>
-                  <label className={style.chooseFileLabel} htmlFor="inputFile">
-                    Selecionar
-                  </label>
-                  <input
-                    id="inputFile"
-                    accept="image/jpeg, image/jpg, image/png, image/webp"
-                    size={60}
-                    className={style.chooseFileButton}
-                    onChange={handleChangeImage}
-                    type="file"
-                  />
-                </div>
-                <div>
-                  <ButtonComponent
-                    style={{
-                      fontSize: '12px',
-                      textTransform: 'none',
-                      backgroundColor: 'var(--blue-400)',
-                      boxShadow: 'none',
-                      border: '1px solid var(--blue-400)',
-                    }}
-                    onClick={openModalCamera}
-                    buttonText="Tirar foto"
-                  />
-                </div>
-              </div>
-              <div className={style.registerButtonContainer}>
-                <ButtonComponent
+          <div className={style.closeButtonContainer}>
+            <ArrowBackRoundedIcon
+              onClick={handleBackModal}
+              fontSize="large"
+              className={style.arrowIcon}
+            />
+            <CloseRoundedIcon
+              onClick={handleCloseModal}
+              fontSize="large"
+              className={style.closeIcon}
+            />
+          </div>
+          <div className={style.registerInformationText}>
+            <h1 className={style.registerTitle}>Cadastrar criança</h1>
+            <p className={style.registerDescription}>
+              Vamos lá! Para cadastrar uma nova criança, basta inserir os dados abaixo.
+            </p>
+          </div>
+          <form className={style.formContainer} onSubmit={handleRegister}>
+            <InputFieldComponent
+              required={true}
+              disabled={loadRegisterData}
+              idInput="name"
+              inputLabel="Nome completo"
+              style={{
+                textTransform: 'capitalize',
+              }}
+              setInputValue={setName}
+              inputValue={name}
+              inputType="text"
+              placeholder="Ex: João da Silva Santos"
+            />
+            <InputFieldComponent
+              disabled={loadRegisterData}
+              required={true}
+              idInput="cpf"
+              inputLabel="CPF (somente números)"
+              setInputValue={(event) => setCpf(maskCpfFunction(event))}
+              inputValue={cpf}
+              inputType="text"
+              placeholder="999.999.999-99"
+            />
+            <InputFieldComponent
+              disabled={loadRegisterData}
+              required={true}
+              idInput="birthDate"
+              inputLabel="Data de nascimento"
+              style={{
+                textTransform: 'capitalize',
+              }}
+              setInputValue={(event) => setBirthDate(maskBirthDateFunction(event))}
+              inputValue={birthDate}
+              inputType="text"
+              placeholder="DD/MM/YYYY"
+            />
+            <SelectComponent
+              required
+              disabled={loadRegisterData}
+              selectId="period"
+              setSelectValue={setPeriod}
+              selectLabel="Período"
+              selectOptions={periodOptions}
+              selectName="period"
+              labelText="Selecione um período"
+            />
+            <SelectComponent
+              required
+              disabled={loadRegisterData}
+              selectId="grade"
+              setSelectValue={setGrade}
+              selectLabel="Turma"
+              selectOptions={gradeOptions}
+              selectName="grade"
+              labelText="Selecione uma turma"
+            />
+            <div
+              style={{
+                backgroundColor: imagePreviewerData ? 'transparent' : 'rgba(0, 0, 0, 0.4)',
+              }}
+              className={style.noPicture}
+            >
+              {!imagePreviewerData ? (
+                <div
                   style={{
-                    fontSize: '1rem',
-                    textTransform: 'none',
-                    backgroundColor: '#00a159',
-                    boxShadow: 'none',
-                    fontWeight: 'bold',
-                    color: '#002F1A',
+                    height: '200px',
+                    display: 'flex',
+                    alignItems: 'center',
                   }}
-                  type="submit"
-                  disabled={
-                    !name ||
-                    cpf.length < 14 ||
-                    !period ||
-                    !grade ||
-                    !imagePreviewerData ||
-                    loadRegisterData
-                  }
-                  buttonText={
-                    loadRegisterData ? (
-                      <CircularProgress
-                        size={28}
-                        style={{
-                          color: '#002F1A',
-                        }}
-                      />
-                    ) : (
-                      'Enviar'
-                    )
-                  }
+                >
+                  <NoPhotographyRoundedIcon
+                    style={{
+                      fontSize: '80px',
+                      color: 'rgba(0, 0, 0, 0.6)',
+                    }}
+                  />
+                </div>
+              ) : (
+                <Image
+                  className={style.imageSelected}
+                  width={100}
+                  height={50}
+                  alt="image"
+                  src={imagePreviewerData}
+                />
+              )}
+            </div>
+            <p
+              style={{
+                fontSize: '10px',
+                visibility: fileName.length ? 'initial' : 'hidden',
+              }}
+            >
+              {fileName.length ? fileName : 'nothing'}
+            </p>
+            <div className={style.containerButtons}>
+              <div className={style.containerFileButton}>
+                <label className={style.chooseFileLabel} htmlFor="inputFile">
+                  Selecionar
+                </label>
+                <input
+                  id="inputFile"
+                  accept="image/jpeg, image/jpg, image/png, image/webp"
+                  size={60}
+                  className={style.chooseFileButton}
+                  onChange={handleChangeImage}
+                  type="file"
                 />
               </div>
-            </form>
-          </div>
+              <div>
+                <ButtonComponent
+                  style={{
+                    fontSize: '12px',
+                    textTransform: 'none',
+                    backgroundColor: 'var(--blue-400)',
+                    boxShadow: 'none',
+                    border: '1px solid var(--blue-400)',
+                  }}
+                  onClick={openModalCamera}
+                  buttonText="Tirar foto"
+                />
+              </div>
+            </div>
+            <div className={style.registerButtonContainer}>
+              <ButtonComponent
+                style={{
+                  fontSize: '1rem',
+                  textTransform: 'none',
+                  backgroundColor: '#00a159',
+                  boxShadow: 'none',
+                  fontWeight: 'bold',
+                  color: '#002F1A',
+                }}
+                type="submit"
+                disabled={
+                  !name ||
+                  cpf.length < 14 ||
+                  !period ||
+                  !grade ||
+                  !imagePreviewerData ||
+                  loadRegisterData
+                }
+                buttonText={
+                  loadRegisterData ? (
+                    <CircularProgress
+                      size={28}
+                      style={{
+                        color: '#002F1A',
+                      }}
+                    />
+                  ) : (
+                    'Enviar'
+                  )
+                }
+              />
+            </div>
+          </form>
         </div>
-      )}
+      </div>
     </>
   );
 }
--- app/components/ModalResponsibleInfoComponent/ModalResponsibleInfoComponent.tsx
+++ app/components/ModalResponsibleInfoComponent/ModalResponsibleInfoComponent.tsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import style from './style.module.css';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import Image from 'next/image';
 import imageError from '@/app/assets/error-image.png';
+import ButtonComponent from '../ButtonComponent/ButtonComponent';
+import ModalRegisterChildComponent from '../ModalRegisterChild/ModalRegisterChildComponent';
 
 type ResponsibleInfo = {
   id: string;
@@ -24,68 +26,99 @@ export default function ModalResponsibleInfoComponent({
   setIsModalResponsibleInfoOpen,
   responsibleInfo,
 }: ModalResponsibleInfoComponentProps) {
+  const [openModalRegisterChild, setOpenModalRegisterChild] = useState(false);
+  const [showResponsibleMoodalContent, setShowResponsibleModalContent] = useState(true);
   const modalRef = useRef(null);
 
   const handleCloseModal = () => {
     setIsModalResponsibleInfoOpen(false);
+    setOpenModalRegisterChild(false);
+    setShowResponsibleModalContent(true);
   };
 
   const handleCloseClickingOutside = (event: any) => {
     if (event.target === modalRef.current) handleCloseModal();
   };
 
+  console.log(responsibleInfo);
+
+  const handleRegisterChild = () => {
+    setShowResponsibleModalContent(false);
+    setOpenModalRegisterChild(true);
+  };
+
+  const backToResponsibleInfo = () => {
+    setShowResponsibleModalContent(true);
+    setOpenModalRegisterChild(false);
+  };
+
   return (
     <>
       {isModalResponsibleInfoOpen && (
         <div onClick={handleCloseClickingOutside} ref={modalRef} className={style.modalBg}>
-          <div className={style.modalBody}>
-            <div className={style.closeButtonContainer}>
-              <CloseRoundedIcon
-                onClick={handleCloseModal}
-                fontSize="large"
-                className={style.closeIcon}
-              />
-            </div>
-            {responsibleInfo && (
-              <>
-                <div className={style.modalContent}>
-                  <div className={style.imgContainer}>
-                    {responsibleInfo.picture ? (
-                      <Image
-                        className={style.responsibleImg}
-                        src={responsibleInfo.picture}
-                        width={600}
-                        height={600}
-                        alt={responsibleInfo.name}
-                      />
-                    ) : (
-                      <Image
-                        className={style.responsibleImg}
-                        src={imageError}
-                        width={600}
-                        height={600}
-                        alt={responsibleInfo.name}
-                      />
-                    )}
-                  </div>
-                  <div className={style.containerInfo}>
-                    <h1 className={style.responsibleName}>{responsibleInfo.name}</h1>
-                    <div className={style.responsibleStats}>
-                      <p>
-                        <span>CPF: </span> {responsibleInfo.cpf}
-                      </p>
-                      <p>
-                        <span>Telefone: </span> {responsibleInfo.phoneNumber}
-                      </p>
-                      <p>
-                        <span>Email: </span> {responsibleInfo.email}
-                      </p>
+          {showResponsibleMoodalContent && !openModalRegisterChild ? (
+            <div className={style.modalBody}>
+              <div className={style.closeButtonContainer}>
+                <CloseRoundedIcon
+                  onClick={handleCloseModal}
+                  fontSize="large"
+                  className={style.closeIcon}
+                />
+              </div>
+              {responsibleInfo && (
+                <>
+                  <div className={style.modalContent}>
+                    <div className={style.imgContainer}>
+                      {responsibleInfo.picture ? (
+                        <Image
+                          className={style.responsibleImg}
+                          src={responsibleInfo.picture}
+                          width={600}
+                          height={600}
+                          alt={responsibleInfo.name}
+                        />
+                      ) : (
+                        <Image
+                          className={style.responsibleImg}
+                          src={imageError}
+                          width={600}
+                          height={600}
+                          alt={responsibleInfo.name}
+                        />
+                      )}
+                    </div>
+                    <div className={style.containerInfo}>
+                      <h1 className={style.responsibleName}>{responsibleInfo.name}</h1>
+                      <div className={style.responsibleStats}>
+                        <p>
+                          <span>CPF: </span> {responsibleInfo.cpf}
+                        </p>
+                        <p>
+                          <span>Telefone: </span> {responsibleInfo.phoneNumber}
+                        </p>
+                        <p>
+                          <span>Email: </span> {responsibleInfo.email}
+                        </p>
+                      </div>
+                      <div style={{ marginTop: '2rem' }}>
+                        <ButtonComponent
+                          onClick={handleRegisterChild}
+                          buttonText="Cadastrar criança"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </>
-            )}
-          </div>
+                </>
+              )}
+            </div>
+          ) : (
+            <div className={style.modalBody}>
+              <ModalRegisterChildComponent
+                handleBackModal={backToResponsibleInfo}
+                isModalOpen={handleCloseModal}
+              />
+            </div>
+          )}
         </div>
       )}
     </>
